ece1674c13b8d68af92253d1b0bdd4892f22a65d
obadh-20 <alghodranobadh2002@gmail.com>
2026-03-04 17:20:09 +0300

    first commit

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { LoginForm } from './components/loginForm'
+const page = () => {
+  return (
+      <div className='flex h-svh items-center'
+      ><LoginForm /></div>
+  )
+}
+
+export default page
---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SignUpForm } from './components/SignUpForm'
+const page = () => {
+  return (
+      <div className='flex h-svh items-center'
+      ><SignUpForm /></div>
+  )
+}
+
+export default page
